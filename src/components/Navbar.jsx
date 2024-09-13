ce39b9ad974e91110271e131e08399cefb957751
CodingCookiee <rzqutubawan@gmail.com>
2024-09-13 20:38:54 +0500

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,15 +22,17 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="Logo" className='w-10 h-10 object-cover rounded-full' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Raza <span className='sm:block hidden'>
-            | Awan
+          <p className='text-white text-[18px]
+           font-bold cursor-pointer flex'>Raza &nbsp; 
+           <span className='sm:block hidden'>
+             | Awan
           </span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link, index) => (
             <li key={index} className={`${
             active === link.title? 'text-white' : 'text-secondary' }'text-white text-[18px] font-medium cursor-pointer
-            hover:text-slate-300  transition duration-300 ease-in-out'`}
+            hover:text-red-600  transition duration-300 ease-in-out'`}
             onClick={() => setActive(link.title)}>
             <a href={`#${link.id}`}>{link.title}
 
@@ -45,8 +47,25 @@ const Navbar = () => {
             className='w-[280px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
           />
-         </div>
+          <div className={`${!toggle?'flex':'hidden'} p-6 black-gradient
+          absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <ul className='list-none flex justify-end items-start flex-col gap-4'>
+          {navLinks.map((link, index) => (
+            <li key={index} className={`${
+            active === link.title? 'text-white' : 'text-secondary' } 
+            font-sans font-medium cursor-pointer text-[16px]`}
+            onClick={() => {
+              setToggle(!toggle);
+              setActive(link.title);
+            }}>
+            <a href={`#${link.id}`}>{link.title}
 
+            </a>
+            </li>
+          ))}
+        </ul>
+          </div>
+         </div>
       </div>
     </nav>
   );
