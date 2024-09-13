16534773c68d7e754d047e65c3991cb016e0d98d
CodingCookiee <rzqutubawan@gmail.com>
2024-09-13 19:58:05 +0500

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { nav } from 'framer-motion/client';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddindx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -20,13 +21,33 @@ const Navbar = () => {
             window.scrollTo(0,0);
           }}
         >
-        <img src={logo} alt="Logo" className='w-10 h-10 object-cover rounded-full' />
-        <p className='text-white text-[18px] font-bold cursor-pointer'>Raza <span className='sm:block hidden'>
-          | Awan
-        </span></p>
+          <img src={logo} alt="Logo" className='w-10 h-10 object-cover rounded-full' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Raza <span className='sm:block hidden'>
+            | Awan
+          </span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link, index) => (
+            <li key={index} className={`${
+            active === link.title? 'text-white' : 'text-secondary' }'text-white text-[18px] font-medium cursor-pointer
+            hover:text-slate-300  transition duration-300 ease-in-out'`}
+            onClick={() => setActive(link.title)}>
+            <a href={`#${link.id}`}>{link.title}
+
+            </a>
+            </li>
+          ))}
+        </ul>
+         <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={ toggle ?menu:close}
+            alt="Menu"
+            className='w-[280px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+         </div>
+
       </div>
     </nav>
   );
-};
-export default Navbar;
+};export default Navbar;
